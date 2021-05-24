33b78b5fe1281138716e56c8184b17f8e215cf14
Pratik <pmpratikmohanty1992@gmail.com>
2021-05-24 07:01:34 +0000

    Modifying hostcheck.sh

--- hostcheck.sh
+++ hostcheck.sh
@@ -1,4 +1,4 @@
-FILE="/home/administrator/hostcheck/serverip.txt"
+FILE1="/home/administrator/hostcheck/serverip.txt"
 while read line; do
 sshpass -p '******' ansible -tt $line -f 1 -k -m shell -a 'cat /etc/hosts' >> /home/administrator/hostcheck/hostcheck_output.txt
 done < $FILE1
